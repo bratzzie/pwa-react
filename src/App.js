25cecb68212281789ec0db16102014c283f11e04
Bichurina Anastasiia <69593629+bratzzie@users.noreply.github.com>
2021-04-09 11:10:43 +0300

    :construction:

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import "./App.css";
-
+import video from "./assets/ballet.mp4";
 function App() {
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const result = await fetch(
+  //       "https://orangevalleycaa.org/api/videos"
+  //     ).then((response) => response.json());
+  //     setData(result);
+  //   };
+  //   fetchData();
+  // }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>Videos</p>
+        <p>Video</p>
       </header>
+
+      {/* {data.map((video) => (
+        <div key={video.id}>
+          <video height={400} controls src={video.src} />
+        </div>
+      ))} */}
+      <video height={"100%"} width={"70%"} controls src={video} />
     </div>
   );
 }
